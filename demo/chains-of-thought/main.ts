a52b7f41334e5f5c29af268d6e9d9f4b451ad444
“chrisshank” <chris.shank.23@gmail.com>
2024-10-16 13:41:25 -0700

    add thought

--- demo/chains-of-thought/main.ts
+++ demo/chains-of-thought/main.ts
@@ -24,6 +24,10 @@ interface ChainOfThought {
 
 const html = String.raw;
 
+function parseHTML(html: string): Element {
+  return document.createRange().createContextualFragment(html).firstElementChild!;
+}
+
 function renderThought(thought: Thought) {
   return html` <spatial-geometry data-id="${thought.id}" x="${thought.x}" y="${thought.y}" width="200" height="100">
     <textarea>${thought.text}</textarea>
@@ -61,6 +65,21 @@ const saveButton = document.querySelector('button[name="save"]')!;
 const saveAsButton = document.querySelector('button[name="save-as"]')!;
 const main = document.querySelector('main')!;
 const fileSaver = new FileSaver('chains-of-thought', 'json', 'application/json');
+
+main.addEventListener('dblclick', (e) => {
+  if (e.target === main) {
+    const spatialGeometry = parseHTML(
+      renderThought({
+        id: String(document.querySelectorAll('spatial-geometry').length + 1),
+        text: '',
+        x: e.clientX,
+        y: e.clientY,
+      })
+    );
+    main.appendChild(spatialGeometry);
+    spatialGeometry.querySelector('textarea')?.focus();
+  }
+});
 
 async function openFile(showPicker = true) {
   try {
