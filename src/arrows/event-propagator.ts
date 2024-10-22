18e6051e2b322a259bfbc6179a05a27983ca9cc0
“chrisshank” <chris.shank.23@gmail.com>
2024-10-22 16:55:36 -0700

    event-propagator

---
+++ src/arrows/event-propagator.ts
@@ -0,0 +1,65 @@
+import { SpatialConnection } from './spatial-connection';
+
+export class EventPropagator extends SpatialConnection {
+  static tagName = 'event-propagator';
+
+  #triggers = (this.getAttribute('triggers') || '').split(',');
+  get triggers() {
+    return this.#triggers;
+  }
+  set triggers(triggers) {
+    this.#triggers = triggers;
+  }
+
+  #expression = '';
+  #function = new Function();
+  get expression() {
+    return this.#expression;
+  }
+  set expression(expression) {
+    this.#expression = expression;
+    this.#function = new Function('$source', '$target', '$event', expression);
+  }
+
+  constructor() {
+    super();
+
+    this.expression = this.getAttribute('expression') || '';
+  }
+
+  observeSource() {
+    super.observeSource();
+
+    for (const trigger of this.#triggers) {
+      // TODO: add special triggers for intersection, rAF, etc.
+      this.sourceElement?.addEventListener(trigger, this.evaluateExpression);
+    }
+
+    this.evaluateExpression();
+  }
+
+  unobserveSource() {
+    super.unobserveSource();
+
+    for (const trigger of this.#triggers) {
+      // TODO: add special triggers for intersection, rAF, etc.
+      this.sourceElement?.removeEventListener(trigger, this.evaluateExpression);
+    }
+  }
+
+  observeTarget() {
+    super.observeTarget();
+    this.evaluateExpression();
+  }
+
+  unobserveTarget() {
+    super.unobserveTarget();
+  }
+
+  // Do we need the event at all?
+  evaluateExpression = (event?: Event) => {
+    if (this.sourceElement === null || this.targetElement === null) return;
+
+    this.#function(this.sourceElement, this.targetElement, event);
+  };
+}
