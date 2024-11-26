092bd1876480a134a3a5dfbcfb6f2cee0180af54
“chrisshank” <chris.shank.23@gmail.com>
2024-11-26 12:45:18 -0800

    autogenerate demo list

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,18 +1,39 @@
 import { resolve } from 'node:path';
 import { readdirSync } from 'node:fs';
-import { defineConfig } from 'vite';
+import { defineConfig, IndexHtmlTransformContext, Plugin } from 'vite';
+
+const files: string[] = readdirSync(__dirname).filter((file) => file.endsWith('.html'));
+
+const input: Record<string, string> = files.reduce((acc, file) => {
+  acc[file.replace('.html', '')] = resolve(__dirname, file);
+  return acc;
+}, {} as Record<string, string>);
+
+const linkGenerator = (): Plugin => {
+  return {
+    name: 'link-generator',
+    transformIndexHtml(html: string, ctx: IndexHtmlTransformContext) {
+      if (!ctx.filename.endsWith('index.html')) return;
+
+      const links = files
+        .filter((file) => !file.includes('index'))
+        .sort()
+        .map((file) => {
+          const title = file.replace('.html', '').replaceAll('-', ' ');
+          return `<li><a href="${file}">${title}</a></li>`;
+        })
+        .join('\n');
+
+      return html.replace('{{ LINKS }}', links);
+    },
+  };
+};
 
 export default defineConfig({
+  plugins: [linkGenerator()],
   build: {
     target: 'esnext',
-    rollupOptions: {
-      input: readdirSync(__dirname)
-        .filter((file) => file.endsWith('.html'))
-        .reduce((acc, file) => {
-          acc[file.replace('.html', '')] = resolve(__dirname, file);
-          return acc;
-        }, {} as Record<string, string>),
-    },
+    rollupOptions: { input },
     modulePreload: {
       polyfill: false,
     },
