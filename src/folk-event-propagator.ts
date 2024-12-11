bf077c5067f3c4abf3471fef9a740848eb8a816f
Orion Reed <orionreedt@gmail.com>
2024-12-11 05:26:02 -0500

    bi demo

--- src/folk-event-propagator.ts
+++ src/folk-event-propagator.ts
@@ -1,8 +1,7 @@
 import { css, PropertyValues } from '@lit/reactive-element';
 import { FolkRope } from './folk-rope.ts';
 import { property } from '@lit/reactive-element/decorators.js';
-import { Propagator } from './common/Propagator.ts';
-import { SymmetricPropagator } from './common/SymmetricPropagator.ts';
+import { BiPropagator } from './common/BiPropagator.ts';
 
 export class FolkEventPropagator extends FolkRope {
   static override tagName = 'folk-event-propagator';
@@ -10,8 +9,15 @@ export class FolkEventPropagator extends FolkRope {
   static styles = [
     ...FolkRope.styles,
     css`
-      textarea {
+      .input-container {
         position: absolute;
+        display: flex;
+        flex-direction: column;
+        transform: translate(-50%, -50%);
+        pointer-events: none;
+      }
+
+      textarea {
         width: auto;
         min-width: 3ch;
         height: auto;
@@ -22,34 +28,61 @@ export class FolkEventPropagator extends FolkRope {
         pointer-events: auto;
         overflow: hidden;
         field-sizing: content;
-        translate: -50% -50%;
-        border-radius: 5px;
+      }
+
+      .input-container textarea:first-child {
+        border-radius: 5px 5px 0 0;
+        border-bottom: none;
+      }
+
+      .input-container textarea:last-child {
+        border-radius: 0 0 5px 5px;
       }
     `,
   ];
 
-  @property({ type: String, reflect: true }) trigger = '';
-  @property({ type: String, reflect: true }) expression = '';
+  @property({ type: String, reflect: true }) sourceTrigger = '';
+  @property({ type: String, reflect: true }) sourceExpression = '';
+  @property({ type: String, reflect: true }) targetTrigger = '';
+  @property({ type: String, reflect: true }) targetExpression = '';
 
-  #triggerTextarea = document.createElement('textarea');
-  #expressionTextarea = document.createElement('textarea');
-  #propagator: SymmetricPropagator | null = null;
+  #sourceTriggerTextarea = document.createElement('textarea');
+  #sourceExpressionTextarea = document.createElement('textarea');
+  #targetTriggerTextarea = document.createElement('textarea');
+  #targetExpressionTextarea = document.createElement('textarea');
+  #propagator: BiPropagator | null = null;
+
+  #sourceContainer = document.createElement('div');
+  #targetContainer = document.createElement('div');
 
   override firstUpdated(changedProperties: PropertyValues<this>): void {
     super.firstUpdated(changedProperties);
 
-    this.#triggerTextarea.addEventListener('change', () => {
-      this.trigger = this.#triggerTextarea.value;
+    this.#sourceTriggerTextarea.addEventListener('change', () => {
+      this.sourceTrigger = this.#sourceTriggerTextarea.value;
+    });
+    this.#sourceExpressionTextarea.addEventListener('input', () => {
+      this.sourceExpression = this.#sourceExpressionTextarea.value;
+    });
+    this.#targetTriggerTextarea.addEventListener('change', () => {
+      this.targetTrigger = this.#targetTriggerTextarea.value;
+    });
+    this.#targetExpressionTextarea.addEventListener('input', () => {
+      this.targetExpression = this.#targetExpressionTextarea.value;
     });
 
-    this.#expressionTextarea.addEventListener('input', () => {
-      this.expression = this.#expressionTextarea.value;
-    });
+    this.#sourceTriggerTextarea.value = this.sourceTrigger;
+    this.#sourceExpressionTextarea.value = this.sourceExpression;
+    this.#targetTriggerTextarea.value = this.targetTrigger;
+    this.#targetExpressionTextarea.value = this.targetExpression;
 
-    this.#triggerTextarea.value = this.trigger;
-    this.#expressionTextarea.value = this.expression;
+    this.#sourceContainer.className = 'input-container';
+    this.#targetContainer.className = 'input-container';
 
-    this.renderRoot.append(this.#triggerTextarea, this.#expressionTextarea);
+    this.#sourceContainer.append(this.#sourceTriggerTextarea, this.#sourceExpressionTextarea);
+    this.#targetContainer.append(this.#targetTriggerTextarea, this.#targetExpressionTextarea);
+
+    this.renderRoot.append(this.#sourceContainer, this.#targetContainer);
 
     this.#initializePropagator();
   }
@@ -57,7 +90,12 @@ export class FolkEventPropagator extends FolkRope {
   override updated(changedProperties: PropertyValues<this>): void {
     super.update(changedProperties);
 
-    if (changedProperties.has('trigger') || changedProperties.has('expression')) {
+    if (
+      changedProperties.has('sourceTrigger') ||
+      changedProperties.has('sourceExpression') ||
+      changedProperties.has('targetTrigger') ||
+      changedProperties.has('targetExpression')
+    ) {
       this.#initializePropagator();
     }
   }
@@ -69,29 +107,38 @@ export class FolkEventPropagator extends FolkRope {
 
   #initializePropagator() {
     this.#propagator?.dispose();
-    this.#propagator = new SymmetricPropagator({
+    this.#propagator = new BiPropagator({
       source: this.sourceElement,
       target: this.targetElement,
-      event: this.trigger,
-      handler: this.expression,
+      sourceEvent: this.sourceTrigger,
+      targetEvent: this.targetTrigger,
+      sourceHandler: this.sourceExpression,
+      targetHandler: this.targetExpression,
       onParseError: () => this.cut(),
       onParseSuccess: () => this.mend(),
     });
+    console.log(this.#propagator);
+    console.log(this.sourceElement);
+    console.log(this.targetElement);
+    console.log(this.sourceTrigger);
+    console.log(this.sourceExpression);
+    console.log(this.targetTrigger);
+    console.log(this.targetExpression);
   }
 
   override render() {
     super.render();
 
-    const triggerPoint = this.points[Math.floor(this.points.length / 5)];
-    if (triggerPoint) {
-      this.#triggerTextarea.style.left = `${triggerPoint.pos.x}px`;
-      this.#triggerTextarea.style.top = `${triggerPoint.pos.y}px`;
+    const sourcePoint = this.points[Math.floor(this.points.length * 0.25)];
+    if (sourcePoint) {
+      this.#sourceContainer.style.left = `${sourcePoint.pos.x}px`;
+      this.#sourceContainer.style.top = `${sourcePoint.pos.y}px`;
     }
 
-    const expressionPoint = this.points[Math.floor(this.points.length / 2)];
-    if (expressionPoint) {
-      this.#expressionTextarea.style.left = `${expressionPoint.pos.x}px`;
-      this.#expressionTextarea.style.top = `${expressionPoint.pos.y}px`;
+    const targetPoint = this.points[Math.floor(this.points.length * 0.75)];
+    if (targetPoint) {
+      this.#targetContainer.style.left = `${targetPoint.pos.x}px`;
+      this.#targetContainer.style.top = `${targetPoint.pos.y}px`;
     }
   }
 }
