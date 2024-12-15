d9c8a8879640ffd85558e58f4b73697fb5f5e06f
Orion Reed <orionreedt@gmail.com>
2024-12-15 13:28:20 -0500

    resize fix

--- src/folk-sand.ts
+++ src/folk-sand.ts
@@ -554,7 +554,7 @@ export class FolkSand extends FolkBaseSet {
       gl.bindTexture(gl.TEXTURE_2D, newTex);
       gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA16F, this.bufferWidth, this.bufferHeight, 0, gl.RGBA, gl.FLOAT, null);
 
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 
